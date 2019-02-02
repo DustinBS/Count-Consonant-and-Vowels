b90d0750b04a19dd45fc4fb10e60b02e34564aca
Dustin <ddbsumarli@gmail.com>
2019-02-02 02:45:20 -0800

    Finished

--- src/CountVowelsAndConsonants.java
+++ src/CountVowelsAndConsonants.java
@@ -3,9 +3,38 @@ Name            Dustin Sumarli
 Class           CS& 141B
 Term            Winter 2019
 Assignment 4    Counts Vowels and Consonants
-Description     This program accepts a number and checks if it is a valid SSN
+Description     This program counts the number of vowels and consonants of a
+                given input.   
 **************************************************/
-// Counts the
+// Counts the amount of vowels and consonants of a given input.
+
+import java.util.Scanner;
 public class CountVowelsAndConsonants {
-    
-}
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        // Prompt for string (also turns the string into lowercase characters)
+        System.out.print("Enter a string: ");
+        String s = input.nextLine();
+        s = s.toLowerCase();
+        
+        // Declare acculumators
+        int vowels = 0;
+        int consonants = 0;
+        
+        for (int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            
+            // count vowels
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                vowels++;
+            } else if (ch >= 'a' && ch <= 'z') { // check if letter
+                consonants++;
+            } // end if
+        } // end for
+        
+        // Display results
+        System.out.println("The number of vowels is " + vowels);
+        System.out.println("The number of consonants is " + consonants);
+    } // end main
+} // end class
