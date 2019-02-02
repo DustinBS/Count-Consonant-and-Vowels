782d3d24d9ba1f5ffe09da5dbbaba174ed5669ba
Dustin <ddbsumarli@gmail.com>
2019-02-02 02:07:39 -0800

    First commit

---
+++ src/CountVowelsAndConsonants.java
@@ -0,0 +1,11 @@
+/**************************************************
+Name            Dustin Sumarli
+Class           CS& 141B
+Term            Winter 2019
+Assignment 4    Counts Vowels and Consonants
+Description     This program accepts a number and checks if it is a valid SSN
+**************************************************/
+// Counts the
+public class CountVowelsAndConsonants {
+    
+}
